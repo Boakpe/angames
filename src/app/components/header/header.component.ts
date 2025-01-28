2722c7fa14a14fbca5d87f7cd0df55f2fd0e7314
Breno <breno.cpedroso@gmail.com>
2025-01-28 20:44:28 -0300

    add login component and user service for authentication management

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  constructor(public userService: UserService) {}
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  private isAdminSubject = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this.isLoggedInSubject.asObservable();
+  isAdmin$ = this.isAdminSubject.asObservable();
+
+  constructor() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      const userData = JSON.parse(user);
+      this.isLoggedInSubject.next(true);
+      this.isAdminSubject.next(userData.admin || false);
+    } else {
+      this.isLoggedInSubject.next(false);
+      this.isAdminSubject.next(false);
+    }
+  }
+}
